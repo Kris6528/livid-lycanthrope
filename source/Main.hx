cb92acbd7e9221ea96b06fbc6fce336d63fa65d8
Zoe.exe <83599055+Kris6528@users.noreply.github.com>
2022-09-03 18:48:48 +0800

    update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -29,6 +29,8 @@ class Main extends Sprite
 
 	public function new()
 	{
+		SUtil.uncaughtErrorHandler();
+		
 		super();
 
 		if (stage != null)
@@ -71,6 +73,9 @@ class Main extends Sprite
 
 		Paths.getModFolders();
 		ClientPrefs.startControls();
+		
+		SUtil.check();
+		
 		addChild(new FlxGame(gameWidth, gameHeight, initialState, zoom, framerate, framerate, skipSplash, startFullscreen));
 
 		#if !mobile
